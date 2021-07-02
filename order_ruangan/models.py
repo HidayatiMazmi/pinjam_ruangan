b5257f9606b55320502028d95f3b80f9141be346
hidayatimazmi <hidayatimazmi@gmail.com>
2021-07-02 15:52:08 +0700

    migrate model and create views

--- order_ruangan/models.py
+++ order_ruangan/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User, Group
 from django.db import models
 
 class Category(models.Model):
@@ -13,8 +13,11 @@ class Category(models.Model):
 class Ruangan(models.Model):
 
     # fields of the model
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    name = models.CharField(max_length=255)
+    alias = models.CharField(max_length=100,null=True)
+    description = models.TextField(blank=True,null=True)
+    image = models.FilePathField(path="/img")
+    available = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='ruangan')
@@ -23,3 +26,15 @@ class Ruangan(models.Model):
     # with their title name
     def __str__(self):
         return self.name
+
+class RuanganTerpinjam(models.Model):
+    ruangan = models.ForeignKey(Ruangan, on_delete=models.CASCADE, related_name='ruangan')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True,null=True)
+    peminjam = models.CharField(max_length=200)
+    tanggal_pinjam = models.DateTimeField()
+    tanggal_selesai = models.DateTimeField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.ruangan
--- order_ruangan/views.py
+++ order_ruangan/views.py
@@ -1,6 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from order_ruangan.models import Ruangan, RuanganTerpinjam, Category
 
 from django.shortcuts import render
 
-# Create your views here.
+def index_ruangan(request):
+    ruangan = Ruangan.objects.all().order_by('-created_on')
+    context = {
+        "ruangan": ruangan,
+    }
+    return render(request,"index_ruangan.html",context)
+
+def ruangan_terpinjam(request):
+    ruangan_terpinjam = RuanganTerpinjam.objects.all().order_by('-created_on')
+    context = {
+        "ruangan_terpinjam" : ruangan_terpinjam,
+    }
+    return render(request, "index_ruangan_terpinjam.html",context)
+
+def category_ruangan(request, category):
+    ruangan = Ruangan.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context = {
+        "category": category,
+        "ruangan": ruangan
+    }
+    return render(request, "category_ruangan.html", context)
+
+def detail_ruangan(request, pk):
+    ruangan = Ruangan.objects.get(pk=pk)
+    
